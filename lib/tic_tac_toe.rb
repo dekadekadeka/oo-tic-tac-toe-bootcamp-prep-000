d26106fade0f8707b2a2055ab38508a95901d862
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-20 18:13:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,7 +99,7 @@ class TicTacToe
   def play
   turn until over?
     if won?
-      puts "Congratulations #{winner(@board)}!"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
